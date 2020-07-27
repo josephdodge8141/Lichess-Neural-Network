6ba35f01b2b960cd72b5169af3f49076e368e82d
skymoney1134 <zappo1134@gmail.com>
2020-07-27 15:14:46 -0600

    invented a new arch that bypasses padding

--- src/model.py
+++ src/model.py
@@ -6,31 +6,83 @@ import createDataSet
 
 labeledData = tf.data.Dataset.from_generator(createDataSet.Generator().gen_data,(tf.string,tf.int32))
 labeledData = labeledData.map(createDataSet.Generator().converter)
-labeledData = labeledData.shuffle(100000, reshuffle_each_iteration=True).batch(512)
+labeledData = labeledData.shuffle(10000, reshuffle_each_iteration=True).batch(512)
 
 valData = tf.data.Dataset.from_generator(createDataSet.Generator().gen_val_data,(tf.string,tf.int32))
-valData = valData.map(createDataSet.Generator().converter).shuffle(100000).batch(512)
-
+valData = valData.map(createDataSet.Generator().converter).shuffle(10000).batch(512)
 
 print('Finished loading data')
 
 
-inputs = tf.keras.Input(shape=(8,8,14))
+inputs = tf.keras.Input(shape=(8,8,35))
+
+a = tf.keras.layers.Conv2D(16,(3,3))(inputs)
+a = tf.keras.layers.BatchNormalization()(a)
+a = tf.keras.layers.ReLU()(a)
+
+b = tf.keras.layers.Conv2D(16,(3,3))(inputs)
+b = tf.keras.layers.BatchNormalization()(b)
+b = tf.keras.layers.ReLU()(b)
+
+a1= tf.keras.layers.Conv2D(16,(3,3))(a)
+a1 = tf.keras.layers.BatchNormalization()(a1)
+a1 = tf.keras.layers.ReLU()(a1)
+
+a2 = tf.keras.layers.Conv2D(16,(3,3))(a)
+a2 = tf.keras.layers.BatchNormalization()(a2)
+a2 = tf.keras.layers.ReLU()(a2)
+
+b1 = tf.keras.layers.Conv2D(16,(3,3))(b)
+b1 = tf.keras.layers.BatchNormalization()(b1)
+b1 = tf.keras.layers.ReLU()(b1)
+
+b2 = tf.keras.layers.Conv2D(16,(3,3))(b)
+b2 = tf.keras.layers.BatchNormalization()(b2)
+b2 = tf.keras.layers.ReLU()(b2)
+
+x = tf.keras.layers.Concatenate(axis=-3)([a1,b1])
+y = tf.keras.layers.Concatenate(axis=-3)([b2,a2])
+z = tf.keras.layers.Concatenate(axis=-2)([x,y])
 
 
-x = tf.keras.layers.Flatten()(inputs)
-x = tf.keras.layers.Dense(64,activation='relu')(x)
-x = tf.keras.layers.Dense(64,activation='relu')(x)
-x = tf.keras.layers.Dense(64,activation='relu')(x)
+
+a = tf.keras.layers.Conv2D(16,(3,3))(z)
+a = tf.keras.layers.BatchNormalization()(a)
+a = tf.keras.layers.ReLU()(a)
+
+b = tf.keras.layers.Conv2D(16,(3,3))(z)
+b = tf.keras.layers.BatchNormalization()(b)
+b = tf.keras.layers.ReLU()(b)
+
+a1= tf.keras.layers.Conv2D(16,(3,3))(a)
+a1 = tf.keras.layers.BatchNormalization()(a1)
+a1 = tf.keras.layers.ReLU()(a1)
+
+a2 = tf.keras.layers.Conv2D(16,(3,3))(a)
+a2 = tf.keras.layers.BatchNormalization()(a2)
+a2 = tf.keras.layers.ReLU()(a2)
+
+b1 = tf.keras.layers.Conv2D(16,(3,3))(b)
+b1 = tf.keras.layers.BatchNormalization()(b1)
+b1 = tf.keras.layers.ReLU()(b1)
+
+b2 = tf.keras.layers.Conv2D(16,(3,3))(b)
+b2 = tf.keras.layers.BatchNormalization()(b2)
+b2 = tf.keras.layers.ReLU()(b2)
+
+x = tf.keras.layers.Concatenate(axis=-3)([a1,b1])
+y = tf.keras.layers.Concatenate(axis=-3)([b2,a2])
+z = tf.keras.layers.Concatenate(axis=-2)([x,y])
+
+
+x = tf.keras.layers.Flatten()(z)
 outputs = tf.keras.layers.Dense(1,activation='tanh')(x)
 
 model = tf.keras.Model(inputs=inputs,outputs=outputs)
 model.summary()
-
 
 model.compile(optimizer=tf.keras.optimizers.Adam(),loss=tf.keras.losses.MeanSquaredError(), metrics=[tf.keras.metrics.MeanAbsoluteError(),tf.keras.metrics.RootMeanSquaredError()])
 
 history = model.fit(labeledData,epochs=3,verbose=1,validation_data=valData)
 
 print(history.history)
-
--- utils/converters/pgnToTensor.py
+++ utils/converters/pgnToTensor.py
@@ -1,16 +1,26 @@
 import pgnToFen
 import tensorflow as tf
+import numpy
 
 class PgnToTensor:
-    def fill_empty_squares(self, str):
+    def fill_empty_squares(self, row):
         line = []
         key = ['p','r','n','b','q','k','P','R','N','B','Q','K',]
-        for i in str:
+        for i in row:
             if i.isdigit():
                 line.extend([0]*int(i))
             else:
                 line.append(key.index(i)+1)
         return line
+
+    def one_hot_vector_to_tensor(self, vector):
+        arr = []
+        for element in vector:
+            temp = tf.fill([8,8,1],element)
+            arr.append(temp)
+
+        return tf.concat([*arr],axis=2)
+
 
     def pgn_to_tensor(self, pgn):
         pgnConverter = pgnToFen.PgnToFen()
@@ -19,10 +29,9 @@ class PgnToTensor:
         boardArray =  [self.fill_empty_squares(i) for i in fen[0].split('/')]
         boardTensor = tf.one_hot(tf.convert_to_tensor(boardArray),13,axis=2,dtype='int32')
         whoToPlay =  1 if fen[1] == 'w' else 0
-        turnTensor = tf.convert_to_tensor([whoToPlay],dtype='int32')
+        turnTensor = self.one_hot_vector_to_tensor([whoToPlay])
         castlingRight = [int(i in fen[3]) for i in ['K','Q','k','q']]
-        castlingTensor = tf.convert_to_tensor(castlingRight)
-        enPassant = 0 if fen[2] == '-' else (ord(fen[2][0]) - ord('a') + 1) + (int(fen[2][1])//6)*8
-        enPassantTensor = tf.one_hot(tf.convert_to_tensor(enPassant),17,dtype='int32')
-        miscData = tf.reshape(tf.concat([(*[turnTensor]*43),castlingTensor, enPassantTensor],axis=0),(8,8,1))
-        return tf.concat([boardTensor,miscData], axis=2)
+        castlingTensor = self.one_hot_vector_to_tensor(castlingRight)
+        enPassant = [0 if fen[2] == '-' else ((ord(fen[2][0]) - ord('a') + 1) + (int(fen[2][1])//6)*8) == i for i in range(17)]
+        enPassantTensor = self.one_hot_vector_to_tensor(enPassant)
+        return tf.concat([boardTensor,turnTensor,castlingTensor,enPassantTensor], axis=2)
